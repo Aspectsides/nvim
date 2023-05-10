92934dd4a6f640cb59e538e8788e37a750225bb6
Daniel Xu <heavydenial@proton.me>
2023-05-10 16:10:59 -0700

    base46

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,9 +1,19 @@
 return {
-  { "Everblush/nvim", name = "everblush" }, -- colorscheme
+  -- { "Everblush/nvim", name = "everblush" }, -- colorscheme
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  { "lmburns/lf.nvim" },
+  { -- Theme inspired by Atom
+    "notken12/base46-colors",
+    priority = 1000,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "everblush",
     },
+  },
+  {
+    "mbbill/undotree",
+    event = "VeryLazy",
   },
 }
